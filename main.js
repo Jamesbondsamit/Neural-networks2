fd0f9e2349174587ac4ace6aa862cc298f5f3c00
Jamesbondsamit <78743840+Jamesbondsamit@users.noreply.github.com>
2021-08-17 16:56:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,27 @@ Webcam.set({
 });
 camera=document.getElementById("camera");
 Webcam.attach('#camera');
-
 function take_snapshot(){
-    Webcam.take_snapshot(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>'; 
-    })
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_img" src="'+data_uri+'"/>';
+    });
 }
 console.log('ml5 version',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Ac98DJJGC/',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gJvkp29Yn/model.json',modelLoaded);
 function modelLoaded(){
-    console.log("Model Loaded")
+    console.log('Model Loaded');
 }
+function check(){
+    img=document.getElementById('capture_img');
+    classifier.classify(img,gotResult);
+}
+function  gotResult(error,results){
+if(error){
+    console.log(error);
+}else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
+}
+
